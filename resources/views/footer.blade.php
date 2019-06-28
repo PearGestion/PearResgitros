ee24e183413ed6a62a61d0d11e724c8205c8821b
Mario Zalazar <mario.zalazar@educacion.local>
2019-06-27 21:29:43 -0300

    Subida de Plantilla Admin LTE

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+        <b>Version</b> 2.4.0
+    </div>
+    <strong>Copyright &copy; 2011-2019 <a href="https://adminlte.io">Pear Gestion</a>.</strong> All rights reserved.
+</footer>
